8f9c078a2c1f8f23dae3841b6bbdf8f7a26e9aa1
SneakyTurtle <le741996cf@gmail.com>
2022-07-12 13:01:04 +0700

    fix B6_2

--- js/main.js
+++ js/main.js
@@ -1,41 +1,58 @@
-var count = 0;
+
 /**
- * Khối 1:
- * count
+ * ?Khối 1:
+ * n, count
  * 
- * Khối 2:
- * while(count < 10)
- *      newDivNode = document.createElement("div");
- *      newDivNode.classList.add("myStyle");
- *      Nếu(count%2 == 0)
- *          newTextNode = document.createTextNode("Div chẵn: " + count);
- *          newDivNode.style.backgroundColor = "red";
- *      Ngược lại
- *          newTextNode = document.createTextNode("Div lẻ: " + count);
- *          newDivNode.style.backgroundColor = "blue";
- *      newDivNode.appendChild(newTextNode);
- *      document.getElementById("resultId").appendChild(newDivNode);
- *      count++;
+ * ?Khối 2:
+ * function isPrime(n)
+ *      *Nếu n < 2 
+ *          return false
+ *      *Ngược lại
+ *          for (let i = 2; i <= Math.sqrt(n); i++)
+ *          Nếu n % i == 0
+ *              return false
+ * return true     
  * 
- * Khối 3:
- * 10 khối div chẵn lẻ
+ * while(count <= n)
+ *      *Nếu isPrime(n)
+ *          result += count + " ";
+ *      count++
+ * 
+ * Thông báo kết quả ra màn hình
+ * 
+ * ?Khối 3:
+ * Dãy nguyên tố
+ * 
  */
-function createDiv() {
-    var newDivNode;
-    var newTextNode;
-    while (count < 10) {
-        newDivNode = document.createElement("div");
-        newDivNode.classList.add("myStyle");
-        if (count % 2 == 0) {
-            newTextNode = document.createTextNode("Div chẵn: " + count);
-            newDivNode.style.backgroundColor = "red";
-        } else {
-            newTextNode = document.createTextNode("Div lẻ: " + count);
-            newDivNode.style.backgroundColor = "blue";
+function showPrime() {
+    var n = document.getElementById("ipNumber").value;
+    var count = 1;
+    var result = "";
+    while (count <= n) {
+        if (isPrime(count)) {
+            result += count + " ";
         }
-        newDivNode.appendChild(newTextNode);
-        document.getElementById("resultId").appendChild(newDivNode);
         count++;
     }
+    if (result === "") {
+        document.getElementById("txtResult").innerHTML =
+            "Không có số nguyên tố nào trong khoảng từ 1 đến " + n;
+    } else {
+        document.getElementById("txtResult").innerHTML =
+            "Dãy nguyên tố: " + result;
+    }
 }
-document.getElementById("btnCreateDiv").onclick = createDiv;
+
+function isPrime(n) {
+    if (n < 2) {
+        return false;
+    } else {
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+document.getElementById("btnShowResult").onclick = showPrime;
